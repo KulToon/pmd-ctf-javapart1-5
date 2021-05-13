81750b5e6191e84b5e50990d7afb7802cf249b78
Lashang166 <bankteeradhol@gmail.com>
2021-05-13 15:09:25 +0700

    'a'

---
+++ src/com/buzzfreeze/helloworld/HelloWorld.java
@@ -0,0 +1,24 @@
+package com.buzzfreeze.helloworld;
+
+public class HelloWorld {
+	public static void main(String [] args) {
+		int score = 81;
+		
+		if(score == 80) {
+			System.out.print("A");
+		}else if(score == 70) {
+			System.out.println("B");
+		}else if(score == 60) {
+			System.out.println("C");
+		}else if(score == 40) {
+			System.out.println("D");
+		}else if(score == 30) {
+			System.out.println("F");
+		}else {
+			System.out.println("E");
+		}
+		
+		
+	}
+	
+}
